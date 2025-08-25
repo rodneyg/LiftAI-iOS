6763145ca6f15dc4b474c6d9b50343d00e45ae33
rodneyg <rodney.gainous@gmail.com>
2025-08-25 15:40:20 -0700

    Initial Commit

---
+++ LiftAI/LiftAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftAIApp.swift
+//  LiftAI
+//
+//  Created by Rodney Gainous Jr on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LiftAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiftAITests/LiftAITests.swift
@@ -0,0 +1,17 @@
+//
+//  LiftAITests.swift
+//  LiftAITests
+//
+//  Created by Rodney Gainous Jr on 8/25/25.
+//
+
+import Testing
+@testable import LiftAI
+
+struct LiftAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
